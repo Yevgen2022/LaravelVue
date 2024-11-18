8e05aac827444121f60f634b54d40f115d32be31
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2024-11-18 22:19:15 +0100

    Learnt methods and data

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,21 +1,34 @@
 <template>
-    <div>
-<SinglePostComponent></SinglePostComponent>
+    <div class="ms-5 mt-3">
+        <SinglePostComponent></SinglePostComponent>
         <p> Name: {{ name }}</p>
         <p> Age: {{ age }}</p>
         <p> isActive: {{ isActive }}</p>
+
+        <button class="btn btn-secondary" @click="updateMessage">{{ message }}</button>
+        <p> Message: {{ message }}</p>
     </div>
 </template>
 
 
 <script setup>
-import SinglePostComponent from "./SinglePostComponent.vue";
-import {ref} from "vue";
 
-const message = ref('Hello, Vue 3!');
-const name = "Alias";
+import SinglePostComponent from "./SinglePostComponent.vue";
+import {computed, ref} from "vue";
+
+
+const message = ref('Message update!');
+const name = ref("Alias");
 let age = 23;
-let isActive = false;
+let isActive = ref(false);
+
+const updateMessage = () => {
+    isActive.value = !isActive.value;
+    message.value = isActive.value ? 'Message true!': 'Message false!';
+};
+
+//const updateName = computed(() => name.value = "Antares")
+
 
 
 
